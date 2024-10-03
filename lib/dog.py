0ae284dcbb922eea44d1088cb8c8f0170c3dbb6d
innocent <iparainnocent@gmail.com>
2024-10-03 16:20:32 +0300

    commit changes

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
-
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name       # Assign name to self.name
+        self.breed = breed     # Assign breed to self.breed, defaults to "Mutt"
+
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
